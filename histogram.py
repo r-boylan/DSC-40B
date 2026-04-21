633fe60e9c03f189038216e48f321dce02209f1f
r-boylan <rboylan@ucsd.edu>
2026-04-21 13:58:26 -0700

    Add files via upload

---
+++ histogram.py
@@ -0,0 +1,10 @@
+def histogram(points, bins):
+    """Efficiently computes a histogram.
+
+    Assumes that both `points` and `bins` are sorted in ascending order to
+    avoid looping through all bins for each point.
+
+    """
+    # TODO: Implement the histogram function
+    
+    return None
---
+++ swap_sum.py
@@ -0,0 +1,11 @@
+def swap_sum(A, B):
+    """Swaps two elements in two sorted arrays to obtain a target sum 
+    difference of 10.
+
+    Assumes that both arrays are sorted in ascending order and only 
+    contain integers.
+
+    """
+    # TODO: Implement the swap_sum function
+    
+    return None
